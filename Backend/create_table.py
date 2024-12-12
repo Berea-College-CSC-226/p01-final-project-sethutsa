59d95a3a3e64d48b73f454f4db9bc4ff6731bf6e
seth_utsa <sethu@berea.edu>
2024-12-11 22:53:39 -0500

    updated database to include a new column called difficulty

--- Backend/create_table.py
+++ Backend/create_table.py
@@ -1,7 +1,4 @@
 import sqlite3
-from optparse import Values
-from tkinter.constants import INSERT
-from unittest.mock import DEFAULT
 
 conn = sqlite3.connect('database.db')
 print("connected to database successfully")
@@ -11,19 +8,37 @@ print("connected to database successfully")
 # print("Table created successfully")
 
 # the data as a list of tuples
-data = [
-    ('Bluegrass Overlook', 0.9, 629),
-    ('East Pinnacle', 1.7, 587),
-    ('West Pinnacle', 1.5, 511),
-    ('Eagle’s Nest', 1.6, 632),
-    ('Buzzard’s Roost', 1.8, 550)
-]
-
-# Inserts the data into the 'trails' table
-conn.executemany('INSERT INTO trails (name, length, elevation) VALUES (?, ?, ?)', data)
-print("Data inserted successfully")
-
-conn.commit()
-
-conn.close()
-print("Connection closed")
+# data = [
+#     ('Bluegrass Overlook', 0.9, 629),
+#     ('East Pinnacle', 1.7, 587),
+#     ('West Pinnacle', 1.5, 511),
+#     ('Eagle’s Nest', 1.6, 632),
+#     ('Buzzard’s Roost', 1.8, 550)
+# ]
+#
+# # Inserts the data into the 'trails' table
+# conn.executemany('INSERT INTO trails (name, length, elevation) VALUES (?, ?, ?)', data)
+# print("Data inserted successfully")
+#
+# cursor = conn.cursor()
+# cursor.execute("ALTER TABLE trails ADD COLUMN difficulty FLOAT;")
+# print("columns added successfully")
+#
+# new_data = [
+#     ('Bluegrass Overlook', 1),
+#     ('East Pinnacle', 1.5),
+#     ('West Pinnacle',2.5),
+#     ('Eagle’s Nest', 2),
+#     ('Buzzard’s Roost', 2)
+# ]
+#
+# cursor.executemany('''
+#             UPDATE trails
+#             SET difficulty = ?
+#             WHERE name = ?;
+#             ''', [(row[1], row[0]) for row in new_data])
+#
+# conn.commit()
+#
+# conn.close()
+# print("Connection closed")
